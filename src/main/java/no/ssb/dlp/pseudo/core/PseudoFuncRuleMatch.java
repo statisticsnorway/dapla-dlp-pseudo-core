c697a6d9ded145a892fe062bac5497467e92fe3a
Kenneth Leine Schulstad <ks@udp.no>
2021-06-30 09:55:27 +0200

    Make PseudoFuncRuleMatch public

--- src/main/java/no/ssb/dlp/pseudo/core/PseudoFuncRuleMatch.java
+++ src/main/java/no/ssb/dlp/pseudo/core/PseudoFuncRuleMatch.java
@@ -5,7 +5,7 @@ import no.ssb.dapla.dlp.pseudo.func.PseudoFunc;
 import no.ssb.dlp.pseudo.core.PseudoFuncRule;
 
 @Value
-class PseudoFuncRuleMatch {
+public class PseudoFuncRuleMatch {
     private final PseudoFunc func;
     private final PseudoFuncRule rule;
 }
