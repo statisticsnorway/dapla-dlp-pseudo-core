622b3c8bee05ae204771b74dd7a0d3ac1806dcad
Bjørn-Andre Skaar <31540110+bjornandre@users.noreply.github.com>
2023-11-12 21:24:43 +0100

    Add github actions. Build to dapla-pseudo-maven repo in AR (#23)
    * Add github actions. Build to dapla-pseudo-maven repo in AR
    * Bump dapla-dlp-pseudo-func.version
    * Fix repository management
    * Rewrite to remove dependencies to avro-buddy and dapla-storage-client

--- src/test/java/no/ssb/dlp/pseudo/core/field/FieldDescriptorTest.java
+++ src/test/java/no/ssb/dlp/pseudo/core/field/FieldDescriptorTest.java
@@ -1,6 +1,5 @@
 package no.ssb.dlp.pseudo.core.field;
 
-import no.ssb.avro.convert.core.FieldDescriptor;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
