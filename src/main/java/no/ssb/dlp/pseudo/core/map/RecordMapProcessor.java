33570759d22c75a00d73aebbfa55c9ad3f738b19
Skaar, Bjørn-Andre <bjorn.skaar@ssb.no>
2024-02-09 08:41:09 +0100

    Refactor RecordMapProcessor to include a more generic metadataProcessor implementation.

--- src/main/java/no/ssb/dlp/pseudo/core/map/RecordMapProcessor.java
+++ src/main/java/no/ssb/dlp/pseudo/core/map/RecordMapProcessor.java
@@ -1,9 +1,9 @@
 package no.ssb.dlp.pseudo.core.map;
 
-import io.reactivex.processors.FlowableProcessor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import no.ssb.dlp.pseudo.core.field.ValueInterceptorChain;
+import org.reactivestreams.Publisher;
 
 import java.util.Map;
 
@@ -11,12 +11,8 @@ import java.util.Map;
 public class RecordMapProcessor<T> {
     private final ValueInterceptorChain valueInterceptorChain;
     @Getter
-    private final MetadataProcessor<T> metadataProcessor;
-    @FunctionalInterface
-    public interface MetadataProcessor<T> {
-        // The MetadataProcessor is used to publish/subscribe to events related to the processing of each RecordMap
-        FlowableProcessor<T> toFlowableProcessor();
-    }
+    private final T metadataProcessor;
+
     public Map<String, Object> init(Map<String, Object> r) {
         return MapTraverser.traverse(r, valueInterceptorChain::init);
     }
